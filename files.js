7a7656b6a29323663c3a47e3794be125d3cc0fc1
Pauladriana <85115054+Pauladriana@users.noreply.github.com>
2021-09-21 18:21:12 -0500

    added function that reads .md files

--- files.js
+++ files.js
@@ -45,14 +45,38 @@ function findMdFile(route) {
 findMdFile(relativeToAbsolutePath(filePath));
 console.log('The MD files are:', mdPaths);
 
+const regexToMatch = {
+  mdLinks: new RegExp(/\[([\w\s\d./?=#&_%~,.:-]+)\]\(((?:\/|https?:\/\/)[\w\d./?=#&_%~,.:-]+)\)/mg),
+  link: new RegExp(/\(((?:\/|https?:\/\/)[\w\d./?=#&_%~,.:-]+)\)/mg),
+  text: new RegExp(/\[([\w\s\d./?=#&_%~,.:-]+)\]/g),
+};
+
 // LEYENDO EL ARCHIVO MD
-/* const fileLinks = [];
-fs.readFile('./lib/archivo.md', 'utf-8', (error, data) => {
-  if (!error) {
-    console.log(data);
-    fileLinks.push(data.split('\n').filter((line) => line.includes('https')));
-    console.log(fileLinks);
-  } else {
-    console.log(error);
-  }
-}); */
+const fileLinks = [];
+const readMdLinks = (arrMdFiles) => {
+  arrMdFiles.forEach((md) => {
+    fs.readFile(md, 'utf-8', (error, fileContent) => {
+      if (!error) {
+        console.log(fileContent, 60);
+        const fileMdContent = fileContent.match(regexToMatch.mdLinks);
+        if (fileMdContent) {
+          fileMdContent.forEach((link) => {
+            const hrefLink = link.match(regexToMatch.link).join().slice(1, -1);
+            const textLink = link.match(regexToMatch.text).join().slice(1, -1);
+            const objLinks = {
+              href: hrefLink,
+              text: textLink,
+              file: md,
+            };
+            fileLinks.push(objLinks);
+          });
+          console.log(fileLinks, 70);
+        }
+      } else {
+        console.log(error, 65);
+      }
+    });
+  });
+};
+
+readMdLinks(mdPaths);
--- lib/anotherLib/archivo3.md
+++ lib/anotherLib/archivo3.md
@@ -1,1 +1,24 @@
 holitas, soy el tercero.
+# Markdown Links
+
+## Índice
+
+- [1. Preámbulo](#1-preámbulo)
+- [2. Resumen del proyecto](#2-resumen-del-proyecto)
+
+## 1. Preámbulo
+
+[Markdown](https:/es.wikipedia.org/wiki/Markdown) es un lenguaje de marcado
+ligero muy popular entre developers.
+
+Dentro de una comunidad de código abierto, nos han propuesto crear una
+herramienta usando [Node.js](https://nodejs.org/), que lea y analice archivos
+en formato `Markdown`, para verificar los links que contengan y reportar
+algunas estadísticas.
+
+![md-links](https://user-images.githubusercontent.com/110297/42118443-b7a5f1f0-7bc8-11e8-96ad-9cc5593715a6.jpg)
+
+## 2. Resumen del proyecto
+
+[Node.js](https://nodejs.org/es/) es un entorno de ejecución para JavaScript
+construido con el [motor de JavaScript V8 de Chrome](https://developers.google.com/v8/).
