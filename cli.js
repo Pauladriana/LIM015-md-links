288abc64db7d3088cc7b4ac15410cae09c2610ec
Pauladriana <85115054+Pauladriana@users.noreply.github.com>
2021-10-04 14:01:27 -0500

    CLI function done

--- cli.js
+++ cli.js
@@ -1,9 +1,10 @@
 /* eslint-disable linebreak-style */
+const chalk = require('chalk');
 const mdLinks = require('./index');
 // CUENTA:
-const stats = (arr) => arr.length;
-const unique = (arr) => new Set(arr).size;
-const broken = (arr) => new Set(arr.filter((link) => link.ok === 'Fail')).size;
+const stats = (arr) => `Total: ${arr.length}`;
+const unique = (arr) => `Unique: ${new Set(arr).size}`;
+const broken = (arr) => `Broken: ${new Set(arr.filter((link) => link.ok === 'Fail')).size}`;
 
 // Obteniendo la ruta
 
@@ -15,25 +16,27 @@ const help = 'Try to write after your path: --stats, --validate, or both';
 if (processArg.length === 5) {
   if (processArg.includes('--stats') && processArg.includes('--validate')) {
     mdLinks(path, { validate: true })
-      .then((res) => console.log(`Total: ${stats(res)}\nUnique: ${unique(res)}\nBroken: ${broken(res)}`))
-      .catch((err) => console.log(err));
+      .then((res) => console.log(`${stats(res)}\n${chalk.cyan(unique(res))}\n${chalk.bold.red(broken(res))}`))
+      .catch((err) => console.log(chalk.red(chalk.white(err))));
   } else {
-    console.log(help);
+    console.log(chalk.bgBlue(chalk.white(help)));
   }
 } else if (processArg.length === 4) {
   if (processArg.includes('--stats')) {
     mdLinks(path, { validate: false })
-      .then((res) => console.log(`Total: ${stats(res)}\nUnique: ${unique(res)}`))
-      .catch((err) => console.log(err));
+      .then((res) => console.log(`${stats(res)}\n${chalk.cyan(unique(res))}`))
+      .catch((err) => console.log(chalk.bgRed(chalk.white(err))));
   } else if (processArg.includes('--validate')) {
     mdLinks(path, { validate: true })
       .then((res) => console.log(res))
-      .catch((err) => console.log(err));
+      .catch((err) => console.log(chalk.bgRed(chalk.white(err))));
   } else {
-    console.log(help);
+    console.log(chalk.bgBlue(chalk.white(help)));
   }
 } else if (processArg.length === 3) {
-  mdLinks(path).then((res) => console.log(res)).catch((err) => console.log(err));
+  mdLinks(path)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(chalk.bgRed(chalk.white(err))));
 } else {
-  console.log(help);
+  console.log(chalk.bgBlue(chalk.white(help)));
 }
